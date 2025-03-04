f42caa0aa7338cd1ee3e45e1f2b05a5f695f9216
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2025-03-04 15:49:14 +0530

    Add files via upload

---
+++ 6.asm
@@ -0,0 +1,37 @@
+	AREA PRG,CODE,READONLY
+START
+	MOV R1,#1
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	ADD R0,R1
+	ADD R1,#1
+	
+	BX LR
+	END
+	
+	
