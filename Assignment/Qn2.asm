bb924da42b6e33bd1435c69f3c2ba66f82b31703
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2025-04-14 16:44:33 +0530

    Update Qn2.asm

--- Assignment/Qn2.asm
+++ Assignment/Qn2.asm
@@ -1,6 +1,5 @@
-AREA Qn2, CODE, READONLY
-    ENTRY
-
+    AREA Qn2, CODE, READONLY
+START
     MOV r0, #0
     MOV r1, #1
     MOV r2, #10
@@ -11,5 +10,5 @@ loop
     MOV r1, r3
     SUBS r2, r2, #1
     BNE loop
-
+    BX LR
     END
