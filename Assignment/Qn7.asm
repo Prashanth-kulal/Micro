82c18e5f32fe9ee46da16f137bbec865fb97cb2f
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2025-04-14 16:50:02 +0530

    Update Qn7.asm

--- Assignment/Qn7.asm
+++ Assignment/Qn7.asm
@@ -1,10 +1,10 @@
-    AREA Qn7, CODE, READONLY
+        AREA Qn7, CODE, READONLY
     ENTRY
 
-    MRS R0, CPSR
-    BIC R0, R0, #0x80
-    MSR CPSR_c, R0
+        MRS R0, CPSR
+        BIC R0, R0, #0x80
+        MSR CPSR_c, R0
 
-    MRS R1, CPSR
-
-    END
+        MRS R1, CPSR
+        BX LR
+        END
