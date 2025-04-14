e47d9bfb6ca2276207f2438b699a9875f3945e33
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2025-04-14 16:46:17 +0530

    Update Qn3.asm

--- Assignment/Qn3.asm
+++ Assignment/Qn3.asm
@@ -1,16 +1,16 @@
-AREA Qn3, CODE, READONLY
+  AREA Qn3, CODE, READONLY
 ENTRY
 
-LDR r0, =nums
-MOV r1, #5
-MOV r2, #0
+  LDR r0, =nums
+  MOV r1, #5
+  MOV r2, #0
 
 loop
-LDR r3, [r0], #4
-MUL r3, r3, r3
-ADD r2, r2, r3
-SUBS r1, r1, #1
-BNE loop
+  LDR r3, [r0], #4
+  MUL r3, r3, r3
+  ADD r2, r2, r3
+  SUBS r1, r1, #1
+  BNE loop
 
 END
 
